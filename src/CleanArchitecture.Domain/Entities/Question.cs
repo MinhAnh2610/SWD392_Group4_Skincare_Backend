a6294061ee6f91dfdae92d463fa4cc3f8a710823
Ung Nguyen Song Phuc <songphucungnguyen@gmail.com>
2025-02-07 11:16:24 +0700

    feat: add entities

---
+++ src/CleanArchitecture.Domain/Entities/Question.cs
@@ -0,0 +1,12 @@
+using CleanArchitecture.Domain.Abstraction;
+
+namespace CleanArchitecture.Domain.Entities;
+
+public class Question : Entity<Guid>
+{
+    public Guid QuizId { get; set; }
+    public string? Title { get; set; }
+    public string? Description { get; set; }
+    public string? Instruction { get; set; }
+    public string? Section { get; set; }
+}
---
+++ src/CleanArchitecture.Domain/Entities/Quiz.cs
@@ -0,0 +1,12 @@
+using CleanArchitecture.Domain.Abstraction;
+
+namespace CleanArchitecture.Domain.Entities;
+
+public class Quiz : Entity<Guid>
+{
+    public string? Title { get; set; }
+    public string? Description { get; set; }
+    public int TargetAgeFrom { get; set; }
+    public int TargetAgeTo { get; set; }
+    public bool IsActive { get; set; }
+}
