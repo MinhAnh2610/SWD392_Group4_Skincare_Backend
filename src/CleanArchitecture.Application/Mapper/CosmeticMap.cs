68165b51eed975545a82a6e9c797dbb8f8f171dd
Phạm Anh Kiệt <kietpase183731@fpt.edu.vn>
2025-02-21 23:31:29 +0700

    Minor improvements

--- src/CleanArchitecture.Application/Mapper/CosmeticMap.cs
+++ src/CleanArchitecture.Application/Mapper/CosmeticMap.cs
@@ -1,15 +1,17 @@
 ﻿using Mapster;
 using CleanArchitecture.Domain.Entities;
 using CleanArchitecture.Application.DTOs.Cosmetic;
-
-public class CosmeticMappingConfig : IRegister
+namespace CleanArchitecture.Application.Mapper
 {
-  public void Register(TypeAdapterConfig config)
+  public class CosmeticMappingConfig : IRegister
   {
-    // Configure the mapping from Cosmetic to CosmeticResponse
-    config.NewConfig<Cosmetic, CosmeticResponse>();
-    config.NewConfig<UpdateCosmetic, CosmeticResponse>();
-    config.NewConfig<CreateCosmetic, CosmeticResponse>();
-    config.NewConfig<Cosmetic, CreateCosmetic>();
+    public void Register(TypeAdapterConfig config)
+    {
+      // Configure the mapping from Cosmetic to CosmeticResponse
+      config.NewConfig<Cosmetic, CosmeticResponse>();
+      config.NewConfig<UpdateCosmetic, CosmeticResponse>();
+      config.NewConfig<CreateCosmetic, CosmeticResponse>();
+      config.NewConfig<Cosmetic, CreateCosmetic>();
+    }
   }
 }
