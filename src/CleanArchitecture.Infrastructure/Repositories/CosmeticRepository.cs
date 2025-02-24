10c4bc1c5efc09a9d2e9b5047ea9f3c02a1794d2
Phạm Hoàng Minh Anh <115133340+MinhAnh2610@users.noreply.github.com>
2025-02-25 04:15:33 +0700

    fix: mapping for subCategory and cosmetic

--- src/CleanArchitecture.Infrastructure/Repositories/CosmeticRepository.cs
+++ src/CleanArchitecture.Infrastructure/Repositories/CosmeticRepository.cs
@@ -5,10 +5,19 @@ namespace CleanArchitecture.Infrastructure.Repositories;
 
 public class CosmeticRepository : GenericRepository<Cosmetic>, ICosmeticRepository
 {
-  public CosmeticRepository(ApplicationDbContext context) : base(context) 
+  public CosmeticRepository(ApplicationDbContext context) : base(context)
   {
 
   }
+  public override async Task<List<Cosmetic>> GetAllAsync()
+  {
+    return await _context.Cosmetics
+      .Include(c => c.SkinType)
+      .Include(c => c.CosmeticSubcategories)
+        .ThenInclude(cs => cs.SubCategory)
+      .ToListAsync();
+  }
+
   public async Task<List<Cosmetic>> GetListByAnyId(Expression<Func<Cosmetic, bool>> predicate)
   {
     var entities = await _context.Set<Cosmetic>().Where(predicate).ToListAsync();
