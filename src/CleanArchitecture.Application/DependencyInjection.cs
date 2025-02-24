e77c3bfced3126c20d4261bd7b4ec16f50e36113
songphuc19102004 <songphucungnguyen@gmail.com>
2025-02-24 18:33:14 +0700

    feat: complete pdf report generation

--- src/CleanArchitecture.Application/DependencyInjection.cs
+++ src/CleanArchitecture.Application/DependencyInjection.cs
@@ -1,19 +1,9 @@
-using CleanArchitecture.Application.DTOs.Auth;
-using CleanArchitecture.Application.DTOs.CouponDTO;
-using CleanArchitecture.Application.DTOs.QuestionDto;
-using CleanArchitecture.Application.DTOs.ReportDto;
-using CleanArchitecture.Application.DTOs.RoleDto;
-using CleanArchitecture.Application.DTOs.UserDto;
 using CleanArchitecture.Application.Interfaces;
 using CleanArchitecture.Application.Services;
 using CleanArchitecture.Application.Strategies;
 using CleanArchitecture.Application.Strategies.BlogFilterStrategy;
 using CleanArchitecture.Application.Validators;
-using CleanArchitecture.Application.Validators.Auth;
-using CleanArchitecture.Application.Validators.Quiz;
 using CleanArchitecture.Application.Validators.Blog;
-using CleanArchitecture.Application.Validators.Role;
-using CleanArchitecture.Application.Validators.User;
 using IdentityServer4.Validation;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -30,21 +20,11 @@ public static class DependencyInjection
 
     QuestPDF.Settings.License = LicenseType.Community;
     
-    // PdfReportGenerateStrategy strategy = new PdfReportGenerateStrategy();
-    // strategy.Generate(new GenerateReportRequest("pdf", "month", DateTime.Now, DateTime.Today));
-    
     services.AddFeatureManagement();
     services.AddHttpContextAccessor();
 
     // Add all validators
     services.AddValidatorsFromAssemblyContaining<CreateBlogRequestValidator>();
-
-    #region Coupon Validatorss
-
-    services.AddScoped<IValidator<QuestionAddRequest>, QuestionAddRequestValidator>();
-    services.AddScoped<IValidator<QuestionUpdateRequest>, QuestionUpdateRequestValidator>();
-
-    #endregion
 
     // Add identity server 4 validator for owner password
     services.AddTransient<IResourceOwnerPasswordValidator, ResourceOwnerPasswordValidator>();
