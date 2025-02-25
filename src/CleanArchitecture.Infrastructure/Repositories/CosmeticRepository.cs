ade1a63983e74382e10588f6268c62639c847ea9
Phạm Anh Kiệt <kietpase183731@fpt.edu.vn>
2025-02-25 10:58:46 +0700

    Fix cosmetic return relationships, minor change controller

--- src/CleanArchitecture.Infrastructure/Repositories/CosmeticRepository.cs
+++ src/CleanArchitecture.Infrastructure/Repositories/CosmeticRepository.cs
@@ -1,4 +1,9 @@
-﻿using CleanArchitecture.Domain.RepositoryContracts;
+﻿using CleanArchitecture.Application.DTOs.Cosmetic;
+using CleanArchitecture.Application.DTOs.CosmeticSubcategory;
+using CleanArchitecture.Domain.Entities;
+using CleanArchitecture.Domain.RepositoryContracts;
+using Mapster;
+using System.Configuration;
 using System.Linq.Expressions;
 
 namespace CleanArchitecture.Infrastructure.Repositories;
@@ -11,13 +16,36 @@ public class CosmeticRepository : GenericRepository<Cosmetic>, ICosmeticReposito
   }
   public async Task<List<Cosmetic>> GetListByAnyId(Expression<Func<Cosmetic, bool>> predicate)
   {
-    var entities = await _context.Set<Cosmetic>().Where(predicate).ToListAsync();
+    var entities = await _context.Set<Cosmetic>()
+      .Where(predicate)
+      .ProjectToType<CosmeticResponse>()
+      .ToListAsync();
     foreach (var entity in entities)
     {
       _context.Entry(entity).State = EntityState.Detached;
     }
-
-    return entities;
+    var cosmetics = entities.Adapt<List<Cosmetic>>();
+    return cosmetics;
   }
+  public override async Task<List<Cosmetic>> GetAllAsync()
+  {
+    var cosmeticsdtos = await _context.Cosmetics
+        .ProjectToType<CosmeticResponse>()  
+        .ToListAsync();
+    var cosmetics = cosmeticsdtos.Adapt<List<Cosmetic>>();
+    return cosmetics;
+
+  }
+  public override async Task<Cosmetic?> GetByIdAsync(Guid id)
+  {
+    var cosmeticsdtos = await _context.Cosmetics
+        .Where(c => c.Id == id)
+        .ProjectToType<CosmeticResponse>()
+        .FirstOrDefaultAsync();
+    var cosmetics = cosmeticsdtos.Adapt<Cosmetic>();
+    return cosmetics;
+
+  }
+
 
 }
