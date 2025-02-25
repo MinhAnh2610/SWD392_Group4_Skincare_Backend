74a58f56a4a653b2de46ca15d7fcf8689ac88762
Phạm Hoàng Minh Anh <115133340+MinhAnh2610@users.noreply.github.com>
2025-02-25 21:33:15 +0700

    chore: use Initial data for blog tags

--- src/CleanArchitecture.Infrastructure/Data/Extensions/DatabaseExtensions.cs
+++ src/CleanArchitecture.Infrastructure/Data/Extensions/DatabaseExtensions.cs
@@ -148,7 +148,7 @@ public static class DatabaseExtensions
             new BlogTag { BlogId = blogs[1].Id, TagId = tags[1].Id }  // Business  
         };
 
-      await context.BlogsTags.AddRangeAsync(blogTags);
+      await context.BlogsTags.AddRangeAsync(InitialData.BlogTags);
       await context.SaveChangesAsync();
     }
   }
