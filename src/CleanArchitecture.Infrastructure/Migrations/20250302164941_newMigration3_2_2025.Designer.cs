026fe124f0247a7acadda88143561d11b28888ca
Thinh An <thinhan1701@gmail.com>
2025-03-02 23:51:57 +0700

    add migration for time stamp

--- src/CleanArchitecture.Infrastructure/Migrations/20250302164941_newMigration3_2_2025.Designer.cs
+++ src/CleanArchitecture.Infrastructure/Migrations/20250302164941_newMigration3_2_2025.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using CleanArchitecture.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace CleanArchitecture.Infrastructure.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250302164941_newMigration3_2_2025")]
+    partial class newMigration3_2_2025
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/CleanArchitecture.Infrastructure/Migrations/20250302164941_newMigration3_2_2025.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CleanArchitecture.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class newMigration3_2_2025 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DeliveryDate",
+                table: "Orders",
+                type: "timestamp",
+                nullable: true,
+                defaultValueSql: "CURRENT_TIMESTAMP",
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp",
+                oldDefaultValueSql: "CURRENT_TIMESTAMP");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DeliveryDate",
+                table: "Orders",
+                type: "timestamp",
+                nullable: false,
+                defaultValueSql: "CURRENT_TIMESTAMP",
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp",
+                oldNullable: true,
+                oldDefaultValueSql: "CURRENT_TIMESTAMP");
+        }
+    }
+}
