5d9f0bc440ce35778557a9145d9d0b2f3c6b7380
Phạm Hoàng Minh Anh <115133340+MinhAnh2610@users.noreply.github.com>
2025-03-05 00:39:57 +0700

    feat: add selling price to order response and create orderItemResponse for the order response items

---
+++ src/CleanArchitecture.Application/DTOs/OrderItemDto/OrderItemResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace CleanArchitecture.Application.DTOs.OrderItemDto;
+
+public class OrderItemResponse
+{
+  public Guid CosmeticId { get; set; }
+  public int Quantity { get; set; }
+  public decimal SellingPrice { get; set; }
+  public decimal SubTotal => SellingPrice * Quantity;
+}
