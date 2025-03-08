c70d89db5f99e4e2d0a3dc7ce2bc9340763d161b
Phạm Hoàng Minh Anh <115133340+MinhAnh2610@users.noreply.github.com>
2025-03-08 18:50:14 +0700

    fix: songphuc db

--- src/CleanArchitecture.Application/DTOs/OrderDto/CreateWalkInOrderRequest.cs
+++ src/CleanArchitecture.Application/DTOs/OrderDto/CreateWalkInOrderRequest.cs
@@ -1,11 +1,8 @@
-using CleanArchitecture.Application.Constants;
-
 namespace CleanArchitecture.Application.DTOs.OrderDto
 {
   public class CreateWalkInOrderRequest : ICreateOrderRequest
   {
     public Dictionary<Guid, int> Cosmetics { get; set; }
-    public Guid? CustomerId { get; set; }
     public string? CustomerPhoneNumber { get; set; }
     public Guid? CouponId { get; set; }
     public string? PaymentMethod { get; set; }
